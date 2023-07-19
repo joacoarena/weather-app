6bba5a0a8413ca8d999ce39575a234f316edc99f
joacoarena <joaquin.t.arena@gmail.com>
2023-07-19 16:58:34 -0300

    starter pack

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import WeatherApp from './componentes/weatherApp'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      
+       <WeatherApp/>
+      
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/componentes/weatherApp.jsx
@@ -0,0 +1,14 @@
+ import React, { useState } from 'react'
+ 
+ export const weatherApp = () => {
+  const [weather, setWeather] = useState(null)
+   return (
+     <div>
+      <form action="">
+        <input type="text" />
+      </form>
+      <div>info</div>
+     </div>
+   )
+ }
+ 
